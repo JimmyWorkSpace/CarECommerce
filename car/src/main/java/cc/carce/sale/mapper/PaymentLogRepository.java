fd6120256f151a422982bb187328c240400e69b7
hagedr <303764416@qq.com>
2025-09-01 09:13:13 +0800

    Revert "Merge branch 'feature/payment-integration' into zhangming"
    This reverts commit 70670af51751e1eaf1003eaf2a0baedeafd1e6ae, reversing
    changes made to 08b8a23213646e9c22240a07b0802efbb426c7bb.

--- car/src/main/java/cc/carce/sale/mapper/PaymentLogRepository.java
+++
@@ -1,8 +0,0 @@
-package cc.carce.sale.mapper;
-
-import cc.carce.sale.entity.PaymentLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PaymentLogRepository extends JpaRepository<PaymentLog, Long> {
-}
