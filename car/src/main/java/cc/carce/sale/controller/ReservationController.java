b3bc4d7c262f2abc9f2b475daf4b670537a8f154
JimmyLiu <wishs900g3@gmail.com>
2025-09-30 10:23:52 +0800

    SMS Integratino Code Fix
    SMS Integration Code fix after talk with the consult

--- car/src/main/java/cc/carce/sale/controller/ReservationController.java
+++ car/src/main/java/cc/carce/sale/controller/ReservationController.java
@@ -3,6 +3,7 @@ package cc.carce.sale.controller;
 import cc.carce.sale.entity.ReservationEntity;
 import cc.carce.sale.service.ReservationService;
 import cc.carce.sale.service.SmsService;
+import cc.carce.sale.service.SmsService.SmsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,15 +30,30 @@ public class ReservationController {
                 "親愛的 %s 您好，預約已完成！時間: %s, 地點: %s",
                 saved.getName(), saved.getDateTime(), saved.getLocation()
         );
-        smsService.sendSms(saved.getMobile(), customerMsg);
+        SmsResponse customerResult = smsService.sendSms(saved.getMobile(), customerMsg);
 
         // 3. Send SMS to dealer
         String dealerMsg = String.format(
                 "有新的預約: 姓名: %s, 電話: %s, 時間: %s, 地點: %s",
                 saved.getName(), saved.getMobile(), saved.getDateTime(), saved.getLocation()
         );
-        smsService.sendSms(DEALER_MOBILE, dealerMsg);
+        SmsResponse dealerResult = smsService.sendSms(DEALER_MOBILE, dealerMsg);
 
-        return "Reservation submitted, saved to DB, and SMS notifications sent.";
+        // 4. Build response message
+        StringBuilder resultMsg = new StringBuilder("Reservation submitted and saved to DB. ");
+
+        if (customerResult.isSuccess()) {
+            resultMsg.append("客戶簡訊發送成功；");
+        } else {
+            resultMsg.append("客戶簡訊發送失敗，原因: ").append(customerResult.getRawResponse()).append("；");
+        }
+
+        if (dealerResult.isSuccess()) {
+            resultMsg.append("車商簡訊發送成功。");
+        } else {
+            resultMsg.append("車商簡訊發送失敗，原因: ").append(dealerResult.getRawResponse());
+        }
+
+        return resultMsg.toString();
     }
 }
